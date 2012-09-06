85ab8e57ff571ce997593189c2e59f8413827fbf
jtnystrom <jtnystrom@gmail.com>
2012-09-06 10:24:11 +0900

    Insert into named graph

--- Tools/src/org/yummydata/store/InsertTTL.java
+++ Tools/src/org/yummydata/store/InsertTTL.java
@@ -3,6 +3,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import org.openrdf.model.impl.URIImpl;
 import org.openrdf.repository.Repository;
 import org.openrdf.repository.RepositoryConnection;
 import org.openrdf.repository.RepositoryException;
@@ -14,11 +15,15 @@ import org.openrdf.rio.RDFHandlerException;
 import org.openrdf.rio.RDFParseException;
 import org.openrdf.rio.turtle.TurtleParser;
 
-
 public class InsertTTL {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException, 
 	RDFHandlerException, RDFParseException, RepositoryException, RepositoryConfigException {
+		if (args.length < 2) {
+			System.err.println("Usage: insertTTL file.ttl http://example.com/graphs/1");
+			System.exit(1);
+		}
+		
 		String url = "http://monomorphic.org:8001/openrdf-sesame";
 		RepositoryManager rm = RemoteRepositoryManager.getInstance(url, "user", "pass");
 		
@@ -27,7 +32,9 @@ public class InsertTTL {
 		RepositoryConnection con = r.getConnection();
 		System.out.println("Got connection");
 		TurtleParser ttl = new TurtleParser();
-		ttl.setRDFHandler(new RDFInserter(con));
+		RDFInserter ins = new RDFInserter(con);
+		ins.enforceContext(new URIImpl(args[1]));
+		ttl.setRDFHandler(ins);
 		System.out.println("Inserting data");
 		ttl.parse(new FileInputStream(args[0]), "http://yummydata.org");
 		con.close();				
